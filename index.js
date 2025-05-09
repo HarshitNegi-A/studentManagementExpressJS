ca0e2c2eec085528dbacbd9af3cc33504d95976d
HarshitNegi-A <harshitnegi226@gmail.com>
2025-05-09 13:15:20 +0530

    sequelize

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express=require('express')
 const app=express()
+const db=require('./utils/db-connection')
 const studentRouter=require('./routes/studentRoutes')
+
+const studentModel=require('./models/students')
+
 app.use(express.json())
 app.use('/students',studentRouter)
 
+db.sync({force:true}).then(()=>{
+    app.listen(3000,()=>console.log("Server is running now"))
+}).catch((err)=>{
+    console.log(err)
+})
 
-app.listen(3000,()=>console.log("Server is running now"))
--- utils/db-connection.js
+++ utils/db-connection.js
@@ -1,31 +1,64 @@
-const mysql=require('mysql2')
+const { Sequelize } = require('sequelize');
 
-const connection=mysql.createConnection({
+const sequelize=new Sequelize('studentManagement','root','Harshit@123',{
     host:'localhost',
-    user:'root',
-    password:'Harshit@123',
-    database:'studentManagement'
-})
+    dialect:'mysql'
+});
 
-connection.connect((err)=>{
-    if(err){
-        console.log(err)
-        return;
-    }
-    console.log("Connection is established")
-    connection.query(`create table if not exists students(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name varchar(20) not null,
-        email varchar(255) not null unique,
-        age int not null
-        )`,(err)=>{
-            if(err){
-                console.log(err)
-            }
-            else{
-                console.log("Table created successfully")
-            }
-        })
-})
+(async()=>{
+    try{
+    await sequelize.authenticate()
+    console.log("Connection to the database has been created")
+}
+catch(err){
+    console.log(err)
+}
+})()
 
-module.exports=connection
+module.exports=sequelize
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mysql=require('mysql2')
+
+// const connection=mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'Harshit@123',
+//     database:'studentManagement'
+// })
+
+// connection.connect((err)=>{
+//     if(err){
+//         console.log(err)
+//         return;
+//     }
+//     console.log("Connection is established")
+//     connection.query(`create table if not exists students(
+//         id INT AUTO_INCREMENT PRIMARY KEY,
+//         name varchar(20) not null,
+//         email varchar(255) not null unique,
+//         age int not null
+//         )`,(err)=>{
+//             if(err){
+//                 console.log(err)
+//             }
+//             else{
+//                 console.log("Table created successfully")
+//             }
+//         })
+// })
+
+// module.exports=connection
